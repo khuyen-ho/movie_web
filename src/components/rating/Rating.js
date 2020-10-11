27a466eff313595bfa48d3bc94ce117d7ea2b53f
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-11 19:25:06 +0700

    fix score rating

--- src/components/rating/Rating.js
+++ src/components/rating/Rating.js
@@ -4,21 +4,25 @@ import StarBorderIcon from "@material-ui/icons/StarBorder";
 import StarHalfIcon from "@material-ui/icons/StarHalf";
 import {withStyles} from '@material-ui/core'
 import style from './styleRating'
+
+const MAX_SCORE = 10;
+
 class Rating extends Component {
   renderStar = () => {
     let rating = [];
     const { danhGia } = this.props.courseItem;
+    let starCount = danhGia/MAX_SCORE*5
 
-    for (let i = 0; i < danhGia; i++) {
+    for (let i = 0; i < starCount; i++) {
       rating.push(<StarIcon fontSize='small' />);
     }
 
-    if ((danhGia * 10) % 10 === 5) {
+    if ((starCount * 10) % 10 === 5) {
       rating.pop();
       rating.push(<StarHalfIcon fontSize='small' />);
     }
 
-    for (let i = 0; i < 5 - danhGia - 0.5; i++) {
+    for (let i = 0; i < 5 - starCount - 0.5; i++) {
       rating.push(<StarBorderIcon fontSize='small' />);
     }
     return rating;
