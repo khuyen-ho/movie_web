958aa4d31782ee7bcca033f2eaa53ccdb3fb7e1b
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-12 09:37:36 +0700

    rename

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Home from "./pages/Home/Home";
+<<<<<<< Updated upstream
+=======
+import Footer from "./components/Footer";
+import Detail from "./pages/Detail/Detail";
+import Booking from "./pages/Booking/Booking";
+import Pagination_ from "./components/Pagination/Pagination";
+import SignIn from "./pages/Sign In/SignIn";
+import SignUp from "./pages/Sign Up/SignUp";
+
+>>>>>>> Stashed changes
 
 class App extends Component {
   render() {
