af513c8f67699a1db614213ca032318f087a97fc
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-12 10:07:49 +0700

    responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,46 @@
-
-import { Box, Container, Typography } from "@material-ui/core";
+import { Box, Container, Grid, Typography } from "@material-ui/core";
 import React, { Component } from "react";
-import style from './style'
-import {withStyles} from '@material-ui/core'
-
+import style from "./style";
+import { withStyles } from "@material-ui/core";
+import HdIcon from "@material-ui/icons/Hd";
+import { connect } from "react-redux";
 class Footer extends Component {
+  renderLogo = () => {
+    return this.props.cinema.map((logo) => <img src={logo.logo} />);
+  };
   render() {
-      const {classes} = this.props
+    const { classes } = this.props;
     return (
       <Box className={classes.foot} py={1}>
         <Container>
-          <Typography component="p">Cao Thanh Long</Typography>
-          <Typography component="p">Ho Lam Bao Khuyen</Typography>
+          <Grid container>
+            <Grid item xs={2} md={3}>
+              <HdIcon />
+            </Grid>
+            <Grid item xs={5} md={4}>
+              <Typography className='title' component="p" variant="subtitle">
+                ĐỐI TÁC
+              </Typography>
+              {this.renderLogo()}
+            </Grid>
+            <Grid item xs={5} md={5}>
+              <Typography className='title' component="p" variant="subtitle">
+                LIÊN HỆ
+              </Typography>
+              <Typography className='phone' component="p">Phone: 0123456789</Typography>
+              <Typography  className='mail' component="p">Gmail: abc12345@gmail.com</Typography>
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     );
   }
 }
-export default withStyles(style)(Footer)
+
+const mapStateToProps = (state) => {
+  return {
+    cinema: state.cinema,
+  };
+};
+
+export default connect(mapStateToProps)(withStyles(style)(Footer));
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -2,7 +2,34 @@ const style = (theme)=>({
     foot:{
         backgroundColor:'#36353a',
         borderTop:'5px solid #c8bc44',
-        color:'#fff'
+        color:'#fff',
+        '& svg':{
+            fontSize:100,
+            '@media screen and (max-width: 768px)': {
+                '&': {
+                    fontSize:70,
+                }
+              }
+        },
+        '& img':{
+            width:30,
+            marginRight:10,
+            '@media screen and (max-width: 768px)': {
+                '&': {
+                    width:25,
+                }
+              }
+        },
+        '& .title':{
+            marginBottom:10
+        },
+        '& .phone,& .mail':{
+            '@media screen and (max-width: 768px)': {
+                '&': {
+                fontSize:13
+                }
+              }
+        },
     }
 })
 
