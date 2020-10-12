f7e6036363291a2e0bf4819edcec264e2c765cc5
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-12 09:26:17 +0700

    rename file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
-import Detail from "./pages/Detail/Detail";
-import Booking from "./pages/Booking/Booking";
-import Pagination_ from "./components/Pagination/Pagination";
-import SignIn from "./pages/Sign In/SignIn";
-import SignUp from "./pages/Sign Up/SignUp";
-
 
 class App extends Component {
-  
-  
-  render(){
-  return (
-    <>
-      {/* <Header /> */}
-      <Home />
-      {/* <Detail/> */}
-      {/* <Booking/> */}
-      {/* <SignIn/> */}
-      {/* <SignUp/> */}
-      {/* <Footer /> */}
-      <div>
-      {/* <Pagination_/> */}
-      </div>
-    </>
-  )}
+  render() {
+    return (
+      <>
+        <Home />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
