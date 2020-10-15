bcaa5b9571c3a8610d4a6c4546a5938d2662c2d9
CaoThanhLong <ctlong26051993@gmail.com>
2020-10-15 15:19:50 +0700

    Bỏ thư mục thừa

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
-import Detail from "./pages/Detail/Detail";
-import Booking from "./pages/Booking/Booking";
-import Pagination_ from "./components/Pagination/Pagination";
-import SignIn from "./pages/Sign In/SignIn";
-import SignUp from "./pages/Sign Up/SignUp";
-
 
 class App extends Component {
-  
-  
-  render(){
-  return (
-    <>
-      {/* <Header /> */}
-      {/* <Home /> */}
-      {/* <Detail/> */}
-      {/* <Booking/> */}
-      {/* <SignIn/> */}
-      <SignUp/>
-      {/* <Footer /> */}
-      <div>
-      {/* <Pagination_/> */}
-      </div>
-    </>
-  )}
+  render() {
+    return <></>;
+  }
 }
 
 export default App;
