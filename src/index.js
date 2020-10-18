988fa1dd760a29d1c87c9ca0dc97281e16e857d5
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-18 23:01:37 +0700

    thêm thư viện react-material-ui-carousel

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from 'react-redux';
-import { ThemeProvider } from '@material-ui/core';
-import store from './redux/store'
-import theme from './theme'
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@material-ui/core";
+import store from "./redux/store";
+import theme from "./theme";
 ReactDOM.render(
   <React.StrictMode>
-     <Provider store={store}>
-      <ThemeProvider theme={theme}> <App /></ThemeProvider>
-     
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
