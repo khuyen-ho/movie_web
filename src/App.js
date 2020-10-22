cc58899bce9e88685e335e99b09c90fe9b79d00d
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-22 12:26:23 +0700

    tạo trang Detail

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
-import SearchMovie from "./components/SearchMovie";
-import Home from "./pages/Home/Home";
-import Film from "./components/Film";
 import Footer from "./components/Footer";
-import Carousel from "./components/Carousel";
+import Detail from "./pages/Detail";
 
 class App extends Component {
   render() {
     return (
       <>
         <Header />
-        <Home />
+        <Detail />
         <Footer />
       </>
     );
