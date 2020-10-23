8622d1b3533338ad228cc88e88fad28ff616270d
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-23 12:21:52 +0700

    tạo component MovieLogoList

---
+++ src/components/ScheduleMovie/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import MovieLogoList from '../MovieLogoList'
+
+export default class ScheduleMovie extends Component {
+    render() {
+        return (
+            <div>
+                <MovieLogoList/>
+            </div>
+        )
+    }
+}
--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ScheduleMovie from '../../components/ScheduleMovie'
 
 export default class Detail extends Component {
     render() {
         return (
             <div>
-                Detail
+                <ScheduleMovie/>
             </div>
         )
     }
