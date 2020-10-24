b35367c814ce51b6c499737dba0d9e1c929637bc
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-10-24 16:39:03 +0700

    lấy ngày hiện tại trong moment

--- src/components/DaysOfWeek/index.jsx
+++ src/components/DaysOfWeek/index.jsx
@@ -1,50 +1,15 @@
-import { Box, Typography, Button, ButtonGroup } from "@material-ui/core";
+import { Box, Button, ButtonGroup } from "@material-ui/core";
 import React, { Component } from "react";
 import useStyles from "./style";
-//import Moment from "react-moment";
 import moment from 'moment';
 
-const dayList = [
-  {
-    maHeThongRap: "BHDStar",
-    tenHeThongRap: "BHD Star Cineplex",
-    biDanh: "bhd-star-cineplex",
-    logo: "http://movie0706.cybersoft.edu.vn/hinhanh/bhd-star-cineplex.png",
-  },
-  {
-    maHeThongRap: "CGV",
-    tenHeThongRap: "cgv",
-    biDanh: "cgv",
-    logo: "http://movie0706.cybersoft.edu.vn/hinhanh/cgv.png",
-  },
-  {
-    maHeThongRap: "CineStar",
-    tenHeThongRap: "CineStar",
-    biDanh: "cinestar",
-    logo: "http://movie0706.cybersoft.edu.vn/hinhanh/cinestar.png",
-  },
-  {
-    maHeThongRap: "Galaxy",
-    tenHeThongRap: "Galaxy Cinema",
-    biDanh: "galaxy-cinema",
-    logo: "http://movie0706.cybersoft.edu.vn/hinhanh/galaxy-cinema.png",
-  },
-  {
-    maHeThongRap: "LotteCinima",
-    tenHeThongRap: "Lotte Cinema",
-    biDanh: "lotte-cinema",
-    logo: "http://movie0706.cybersoft.edu.vn/hinhanh/lotte-cinema.png",
-  },
-  {
-    maHeThongRap: "MegaGS",
-    tenHeThongRap: "MegaGS",
-    biDanh: "megags",
-    logo: "http://movie0706.cybersoft.edu.vn/hinhanh/megags.png",
-  },
-];
-
-const Day = (dayList) => {
+const Day = () => {
+  const dayList = [];
   const styles = useStyles();
+  for(let i = 0; i<7; i++){
+    dayList.push({weekDay: moment().date(moment().date()+i).format('dddd Do')})
+  }
+  console.log(dayList);
   return (
     <ButtonGroup
       color="secondary"
@@ -53,23 +18,19 @@ const Day = (dayList) => {
     >
       {dayList.map((item, index) => (
         <Button className={styles.logo} key={index}>
-          <img className="img" src={item.logo} />
+         {item.weekDay}
         </Button>
       ))}
     </ButtonGroup>
   );
 };
 
-const Time = () => {
-console.log(moment().date(moment().date()+1).format());
-};
 
 const DaysOfWeek = (props) => {
   const styles = useStyles();
   return (
     <Box className={styles.container} my={5} mx={2} border="1px solid grey">
-      {/* {Day(dayList)} */}
-      {Time()}
+      {Day()}
     </Box>
   );
 };
