b57f703a998551f93ae1ff069c8925639e451a63
CaoThanhLong <ctlong26051993@gmail.com>
2020-11-13 17:08:39 +0700

    Cập nhật lại responsive cho phần SearchMovie

--- src/components/SearchMovie/index.jsx
+++ src/components/SearchMovie/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Grid } from "@material-ui/core";
+import { Button, Container, Grid, Box } from "@material-ui/core";
 import useStyle from "./style";
 import DropDown from "../DropDown";
 
@@ -14,41 +14,37 @@ const SearchMovie = (props) => {
   ];
   const cinemaList = ["Chọn rạp", "Rạp 1", "Rạp 2", "Rạp 3", "Rạp 4"];
   const dateList = ["Chọn ngày xem", "Ngày 1", "Ngày 2", "Ngày 3", "Ngày 4"];
-  const showTimeList = [
-    "Chọn suất chiếu",
-    "Suất 1",
-    "Suất 2",
-    "Suất 3",
-    "Suất 4",
-  ];
+  const showTimeList = ["Chọn suất", "Suất 1", "Suất 2", "Suất 3", "Suất 4"];
 
   const styles = useStyle();
 
   return (
-    <Container fixed classes={{ fixed: styles.container }}>
-      <Grid container spacing={1}>
-        <Grid item xs={12} lg={4}>
-          <DropDown list={movieList} />
+    <Container maxWidth="lg">
+      <Box className={styles.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} lg={4}>
+            <DropDown list={movieList} />
+          </Grid>
+          <Grid item xs={12} lg={2}>
+            <DropDown list={cinemaList} />
+          </Grid>
+          <Grid item xs={12} lg={2}>
+            <DropDown list={dateList} />
+          </Grid>
+          <Grid item xs={12} lg={2}>
+            <DropDown list={showTimeList} />
+          </Grid>
+          <Grid item xs={12} lg={2}>
+            <Button
+              variant="contained"
+              color="secondary"
+              classes={{ root: styles.button }}
+            >
+              MUA VÉ NGAY
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12} lg={2}>
-          <DropDown list={cinemaList} />
-        </Grid>
-        <Grid item xs={12} lg={2}>
-          <DropDown list={dateList} />
-        </Grid>
-        <Grid item xs={12} lg={2}>
-          <DropDown list={showTimeList} />
-        </Grid>
-        <Grid item xs={12} lg={2}>
-          <Button
-            variant="contained"
-            color="secondary"
-            classes={{ root: styles.button }}
-          >
-            MUA VÉ NGAY
-          </Button>
-        </Grid>
-      </Grid>
+      </Box>
     </Container>
   );
 };
--- src/components/SearchMovie/style.js
+++ src/components/SearchMovie/style.js
@@ -1,28 +1,15 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyle = makeStyles((theme) => ({
-  container: {
+  root: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(2),
     height: "100%",
-    margin: "auto",
     backgroundColor: "white",
     borderRadius: 5,
     boxShadow: "0 0 10px rgba(0,0,0,.3)",
-    padding: theme.spacing(2.5, 2),
-    [theme.breakpoints.up("xs")]: {
-      display: "none",
-    },
-    [theme.breakpoints.up("sm")]: {
-      maxWidth: 500,
-      display: "flex",
-      marginTop: theme.spacing(3),
-    },
-    [theme.breakpoints.up("md")]: {
-      maxWidth: 700,
-    },
-    [theme.breakpoints.up("lg")]: {
-      maxWidth: 950,
-    },
   },
+
   button: {
     outline: "none",
     color: theme.palette.white,
