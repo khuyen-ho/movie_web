590c8a0d00e0c4a25284639c9b6a0a46846059a5
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-11-17 13:38:16 +0700

    chỉnh css phần headerBooking

--- src/components/HeaderBooking/index.jsx
+++ src/components/HeaderBooking/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import useStyles, { CssTabs } from "./style";
+import { Box, Typography } from "@material-ui/core";
 
 export default function HeaderBooking() {
   const [value, setValue] = React.useState(2);
@@ -10,19 +12,22 @@ export default function HeaderBooking() {
     setValue(newValue);
   };
 
+  const classes = useStyles();
   return (
-    <Paper square>
-      <Tabs
+    <Paper className={classes.title} square>
+      <CssTabs
         value={value}
         indicatorColor="primary"
         textColor="primary"
         onChange={handleChange}
         aria-label="disabled tabs example"
       >
-        <Tab label="Active" />
-        <Tab label="Disabled" disabled />
-        <Tab label="Active" />
-      </Tabs>
+        <Tab label="CHỌN GHẾ & THANH TOÁN" />
+        <Tab label="KẾT QUẢ ĐẶT VÉ" disabled />
+      </CssTabs>
+      <Box>
+        <Typography variant='h6'>Họ tên</Typography>
+      </Box>
     </Paper>
   );
 }
---
+++ src/components/HeaderBooking/style.js
@@ -0,0 +1,21 @@
+const { makeStyles, Tabs, withStyles } = require("@material-ui/core");
+
+const useStyles = makeStyles((theme) => ({
+   title:{
+     display:'flex',
+     justifyContent:"space-between",
+     alignItems:"center",
+     paddingLeft:theme.spacing(3),
+     paddingRight:theme.spacing(3),
+   }
+  }));
+
+
+  export const CssTabs = withStyles((theme)=>({
+    root: {
+      "& .Mui-selected": {
+      color:theme.palette.secondary.main
+      },
+    },
+  }))(Tabs);
+  export default useStyles
