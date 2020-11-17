3d54720ba47ddbc456f2a1f32acb9305c0370a32
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-11-17 12:52:43 +0700

    chỉnh css cho phần ChairArea

--- src/components/SeatTable/index.jsx
+++ src/components/SeatTable/index.jsx
@@ -4,6 +4,44 @@ import WeekendIcon from "@material-ui/icons/Weekend";
 import IconButton from "@material-ui/core/IconButton";
 import useStyles from "./style";
 
+const chairList = [
+  { SoGhe: 1, TenGhe: "số 1 ", Gia: 100, TrangThai: false },
+  { SoGhe: 2, TenGhe: "số 2 ", Gia: 100, TrangThai: false },
+  { SoGhe: 3, TenGhe: "số 3 ", Gia: 100, TrangThai: false },
+  { SoGhe: 4, TenGhe: "số 4 ", Gia: 100, TrangThai: false },
+  { SoGhe: 5, TenGhe: "số 5 ", Gia: 100, TrangThai: false },
+  { SoGhe: 6, TenGhe: "số 6 ", Gia: 100, TrangThai: false },
+  { SoGhe: 7, TenGhe: "số 7 ", Gia: 100, TrangThai: false },
+  { SoGhe: 8, TenGhe: "số 7 ", Gia: 100, TrangThai: false },
+  { SoGhe: 9, TenGhe: "số 9 ", Gia: 100, TrangThai: false },
+  { SoGhe: 10, TenGhe: "số 10 ", Gia: 100, TrangThai: false },
+  { SoGhe: 11, TenGhe: "số 11 ", Gia: 100, TrangThai: false },
+  { SoGhe: 12, TenGhe: "số 12 ", Gia: 100, TrangThai: false },
+  { SoGhe: 13, TenGhe: "số 13 ", Gia: 100, TrangThai: false },
+  { SoGhe: 14, TenGhe: "số 14 ", Gia: 100, TrangThai: false },
+  { SoGhe: 15, TenGhe: "số 15 ", Gia: 100, TrangThai: false },
+  { SoGhe: 16, TenGhe: "số 16 ", Gia: 100, TrangThai: false },
+  { SoGhe: 17, TenGhe: "số 17 ", Gia: 100, TrangThai: false },
+  { SoGhe: 18, TenGhe: "số 18 ", Gia: 100, TrangThai: false },
+  { SoGhe: 19, TenGhe: "số 19 ", Gia: 100, TrangThai: false },
+  { SoGhe: 20, TenGhe: "số 20 ", Gia: 100, TrangThai: false },
+  { SoGhe: 21, TenGhe: "số 21 ", Gia: 100, TrangThai: false },
+  { SoGhe: 22, TenGhe: "số 22 ", Gia: 100, TrangThai: false },
+  { SoGhe: 23, TenGhe: "số 23 ", Gia: 100, TrangThai: false },
+  { SoGhe: 24, TenGhe: "số 24 ", Gia: 100, TrangThai: false },
+  { SoGhe: 25, TenGhe: "số 25 ", Gia: 100, TrangThai: false },
+  { SoGhe: 26, TenGhe: "số 26 ", Gia: 100, TrangThai: false },
+  { SoGhe: 27, TenGhe: "số 27 ", Gia: 100, TrangThai: false },
+  { SoGhe: 28, TenGhe: "số 28 ", Gia: 100, TrangThai: false },
+  { SoGhe: 29, TenGhe: "số 29 ", Gia: 100, TrangThai: false },
+  { SoGhe: 30, TenGhe: "số 30 ", Gia: 100, TrangThai: true },
+  { SoGhe: 31, TenGhe: "số 31 ", Gia: 100, TrangThai: false },
+  { SoGhe: 32, TenGhe: "số 32 ", Gia: 100, TrangThai: false },
+  { SoGhe: 33, TenGhe: "số 33 ", Gia: 100, TrangThai: false },
+  { SoGhe: 34, TenGhe: "số 34 ", Gia: 100, TrangThai: false },
+  { SoGhe: 35, TenGhe: "số 35 ", Gia: 100, TrangThai: false },
+];
+
 const SeatTable = (props) => {
   const classes = useStyles();
   return (
@@ -19,7 +57,7 @@ const SeatTable = (props) => {
                 />
               </div>
               <div>
-                <Typography>
+                <Typography variant="h6">
                   BHD Star <span> - Vincom 3/2</span>
                 </Typography>
                 <Typography>- 21:35 - RẠP 4</Typography>
@@ -34,37 +72,37 @@ const SeatTable = (props) => {
           <img src="./imgs/screen.png" alt="" />
         </Grid>
         <Grid item xs={12}>
-          <IconButton aria-label="delete">
-            <WeekendIcon />
-          </IconButton>
-          <IconButton aria-label="delete">
-            <WeekendIcon />
-          </IconButton>
-          <IconButton aria-label="delete">
-            <WeekendIcon />
-          </IconButton>
+          <Box className={classes.seatArea}>
+            <Grid container>
+              {chairList.map((item, index) => (
+                <Grid item xs={1} key={index}>
+                  <IconButton aria-label="delete">
+                    <WeekendIcon />
+                  </IconButton>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
         </Grid>
         <Grid item xs={12}>
-          <Box>
-            <WeekendIcon />
-            <Typography component="span">Ghế đang chọn</Typography>
+          <Box className={classes.note}>
+            <div className={classes.noteChair}>
+              <WeekendIcon />
+              <Typography component="span">Ghế đang chọn</Typography>
+            </div>
+            <div className={classes.noteChair}>
+              <WeekendIcon />
+              <Typography component="span">Ghế đã chọn</Typography>
+            </div>
+            <div className={classes.noteChair}>
+              <WeekendIcon />
+              <Typography component="span">Ghế không thể chọn</Typography>
+            </div>
+            <div className={classes.noteChair}>
+              <WeekendIcon />
+              <Typography component="span">Ghế VIP</Typography>
+            </div>
           </Box>
-          <div>
-            <WeekendIcon />
-            <Typography component="span">Ghế đang chọn</Typography>
-          </div>
-          <div>
-            <WeekendIcon />
-            <Typography component="span">Ghế đang chọn</Typography>
-          </div>
-          <div>
-            <WeekendIcon />
-            <Typography component="span">Ghế đang chọn</Typography>
-          </div>
-          <div>
-            <WeekendIcon />
-            <Typography component="span">Ghế đang chọn</Typography>
-          </div>
         </Grid>
       </Grid>
     </Box>
--- src/components/SeatTable/style.js
+++ src/components/SeatTable/style.js
@@ -3,7 +3,9 @@ import { makeStyles, Table, TableContainer, withStyles } from "@material-ui/core
 const useStyles = makeStyles((theme) => ({
  title:{
      display:"flex",
-     justifyContent:"space-between"
+     justifyContent:"space-between",
+     marginTop:theme.spacing(3),
+     marginBottom:theme.spacing(3)
  },
     leftTitle:{
       display:"flex",
@@ -12,9 +14,23 @@ const useStyles = makeStyles((theme) => ({
   },
   logo:{
       height:70,
+      marginRight:theme.spacing(2),
       "& img":{
           height:"100%"
       }
+  },
+  seatArea:{
+      marginRight:theme.spacing(45),
+      marginLeft:theme.spacing(45),
+      marginTop:theme.spacing(5),
+  },
+  note:{
+      display:'flex',
+      justifyContent:"center"
+  },
+  noteChair:{
+      paddingRight:theme.spacing(2),
+      marginTop:theme.spacing(5)
   }
   
 }));
