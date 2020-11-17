617998341db82df071152b176f62bcac84e66ca8
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2020-11-17 13:23:17 +0700

    chỉnh css phần PriceTable

--- src/components/PriceTable/index.jsx
+++ src/components/PriceTable/index.jsx
@@ -1,85 +1,82 @@
-// const PriceTable = (props) => {
-//   return <div>
-//       PriceTable
-//   </div>;
-// };
-// export default PriceTable;
 import React from "react";
-//import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { Button, Typography } from "@material-ui/core";
-import useStyles, { CssTable, CssTableContainer } from './style'
+import { Box, Typography } from "@material-ui/core";
+import useStyles, { CssButton, CssInput, CssTableContainer } from "./style";
 
-// const useStyles = makeStyles({
-//   table: {
-//     maxWidth: 650,
-//   },
-// });
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+const chosenChair = [
+  { SoGhe: 1, TenGhe: "số 1 ", Gia: 100, TrangThai: false },
+  { SoGhe: 2, TenGhe: "số 2 ", Gia: 100, TrangThai: false },
 ];
 
-export default function PriceTable() {
+const PriceTable = (props) => {
   const classes = useStyles();
-
   return (
     <CssTableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableBody>
           <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Typography component='h4'>0đ</Typography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Typography component='h4'>Title</Typography>
-                <Typography component='h4'>Cinema</Typography>
-                <Typography component='h4'>Time</Typography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Typography component='h4'>Ghế</Typography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Typography component='h4'>Email</Typography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Typography component='h4'>Phone</Typography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Typography component='h4'>Phone</Typography>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="center" component="th" scope="row">
-                <Button fullWidth>ĐẶT VÉ</Button>
-              </TableCell>
-            </TableRow>
+            <TableCell align="center" component="th" scope="row">
+              <Typography className={classes.price} variant="h2">
+                0đ
+              </Typography>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center" component="th" scope="row">
+              <Box className={classes.detail}>
+                <Typography variant="h5">Title</Typography>
+                <Typography component="h4">BHD Cinema</Typography>
+                <Typography component="h4">23:15 24/12/2002</Typography>
+              </Box>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center" component="th" scope="row">
+              <Box className={classes.chosenChair}>
+                <Typography component="h4">Ghế đã chọn: </Typography>
+                {chosenChair.map((item, index) => (
+                  <Typography key={index}>{item.TenGhe}</Typography>
+                ))}
+              </Box>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center" component="th" scope="row">
+              <CssInput
+                className={classes.email}
+                placeholder="Email"
+                inputProps={{ "aria-label": "description" }}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center" component="th" scope="row">
+            <CssInput
+                className={classes.phone}
+                placeholder="Phone"
+                inputProps={{ "aria-label": "description" }}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center" component="th" scope="row">
+              <CssButton fullWidth>
+                <Typography variant='h5'>
+                ĐẶT VÉ
+                </Typography>
+              </CssButton>
+            </TableCell>
+          </TableRow>
         </TableBody>
       </Table>
     </CssTableContainer>
   );
-}
+};
+
+export default PriceTable;
--- src/components/PriceTable/style.js
+++ src/components/PriceTable/style.js
@@ -1,17 +1,55 @@
-import { makeStyles, Table, TableContainer, withStyles } from "@material-ui/core";
+import {
+  Button,
+  Input,
+  makeStyles,
+  Table,
+  TableContainer,
+  withStyles,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  
-  
+  price: {
+    color: theme.palette.green.main,
+  },
+  detail: {
+    textAlign: "left",
+    paddingLeft: theme.spacing(3),
+  },
+  chosenChair: {
+    textAlign: "left",
+    paddingLeft: theme.spacing(3),
+  },
+  email: {
+    paddingLeft: theme.spacing(3),
+  },
+  phone: { paddingLeft: theme.spacing(3) },
 }));
-  
-export const CssTableContainer = withStyles({
-    root: {
-        "&.MuiTableContainer-root":{
-        //width:"25%",
-        height:"100vh"
-        }
-    },
-  })(TableContainer);
 
-export default useStyles
+export const CssTableContainer = withStyles({
+  root: {
+    "&.MuiTableContainer-root": {
+      //width:"25%",
+      height: "100vh",
+    },
+  },
+})(TableContainer);
+
+export const CssInput = withStyles({
+  root: {
+    "&.MuiInput-underline::before": {
+      borderBottom: "none",
+    },
+  },
+})(Input);
+
+export const CssButton = withStyles((theme) => ({
+  root: {
+    "&.MuiButtonBase-root": {
+      background: theme.palette.secondary.main,
+      color:theme.palette.white,
+      height:60
+    },
+  },
+}))(Button);
+
+export default useStyles;
