dd42377c3616073a65dcb3a4fca25135791cfd34
CaoThanhLong <48120066+CaoThanhLong@users.noreply.github.com>
2020-12-25 10:40:16 +0700

    Update style.js
    Chỉnh cho CinemInfo canh giữa

--- src/components/CinemaInfo/style.js
+++ src/components/CinemaInfo/style.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    alignItem: "center",
     padding: theme.spacing(1, 0),
 
     "&:hover": {
