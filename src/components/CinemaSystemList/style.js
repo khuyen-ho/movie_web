fc9a65402401942378ade5e31af149b0be5a6a9b
CaoThanhLong <ctlong26051993@gmail.com>
2020-12-30 14:29:41 +0700

    Cập nhật lại transtion cho CinemaSystemList

--- src/components/CinemaSystemList/style.js
+++ src/components/CinemaSystemList/style.js
@@ -6,6 +6,7 @@ const useStyles = makeStyles((theme) => ({
     borderBottom: `1px solid ${theme.palette.grey.light}`,
     opacity: 0.5,
     background: "none!important",
+    transition: "all 100ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
     "&:hover": {
       cursor: "pointer",
       opacity: 1.0,
