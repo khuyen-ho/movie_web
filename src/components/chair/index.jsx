73044ad0fe141b429ae2fecb4246f032d225c785
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-01-07 02:11:13 +0700

    chỉnh lại style cho component Chair

--- src/components/chair/index.jsx
+++ src/components/chair/index.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import WeekendIcon from "@material-ui/icons/Weekend";
-import IconButton from "@material-ui/core/IconButton";
+import useStyles from "./style";
 import { CssIconButton } from "./style";
 
 const Chair = (props) => {
+  const classes = useStyles();
+  let chosing = false;
+  let name;
+  if (props.daDat) name = classes.chosen;
+  else if (props.loaiGhe === "Vip") name = classes.vip;
+  else name = classes.chair;
+
   return (
-      <span>
-        <CssIconButton aria-label="delete">
-          <WeekendIcon />
-        </CssIconButton>
-      </span>
+    <span>
+      <CssIconButton aria-label="delete" className={name}>
+        {"" || props.tenGhe}
+      </CssIconButton>
+    </span>
   );
 };
 
-export default Chair
+export default Chair;
--- src/components/chair/style.js
+++ src/components/chair/style.js
@@ -1,15 +1,36 @@
-const { makeStyles, Tabs, withStyles, IconButton } = require("@material-ui/core");
+const { makeStyles, withStyles, IconButton } = require("@material-ui/core");
 
 const useStyles = makeStyles((theme) => ({
-  title: {
+  chair:{
+    backgroundColor:theme.palette.grey.light,
   },
+  
+  chosen:{
+    backgroundColor:"red",
+    cursor:"no-drop",
+    pointerEvents:"none",
+  },
+  isChosing:{
+    backgroundColor:theme.palette.green.main
+  },
+  vip:{
+    backgroundColor:"orange"
+  }
+
 }));
 
 export const CssIconButton = withStyles((theme) => ({
   root: {
     "&.MuiIconButton-root": {
       outline: "none",
+      // backgroundColor:theme.palette.grey.light,
+      borderRadius: 6,
+      width:40,
+      height:40,
+      fontSize:20,
+
     },
+    
   },
 }))(IconButton);
 export default useStyles;
