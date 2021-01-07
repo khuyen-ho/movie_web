a6addc54919062f55b981437f8317d4f1354a87e
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-01-08 00:59:47 +0700

    tạo component MovieManagement

--- src/components/MovieManagement/index.jsx
+++ src/components/MovieManagement/index.jsx
@@ -1,11 +1,115 @@
-import React, { Component } from 'react'
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import CreateIcon from "@material-ui/icons/Create";
+import useStyles, {
+  CssIconButton,
+  StyledTableCell,
+  StyledTableRow,
+} from "./style";
+import BlockIcon from "@material-ui/icons/Block";
+import SearchBar from "../SearchBar";
+import { Box } from "@material-ui/core";
+const movieList = [
+  {
+    maPhim: 1314,
+    tenPhim: "13 Reasons Why",
+    biDanh: "13-reasons-why",
+    trailer: "https://www.youtube.com/embed/1HpZevFifuo",
+    hinhAnh:
+      "http://movie0706.cybersoft.edu.vn/hinhanh/13-reasons-why_gp01.png",
+    moTa:
+      "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, plan and pull off a heist that will save the world.",
+    maNhom: "GP01",
+    ngayKhoiChieu: "2020-12-21T00:00:00",
+    danhGia: 10,
+  },
+  {
+    maPhim: 1329,
+    tenPhim: "Dao Kinh Hoang 12",
+    biDanh: "dao-kinh-hoang-12",
+    trailer: "https://www.youtube.com/embed/IHNzOHi8sJs",
+    hinhAnh:
+      "http://movie0706.cybersoft.edu.vn/hinhanh/dao-kinh-hoang-12_gp01.jpg",
+    moTa: "Người càng xinh đẹp, càng dễ lừa dối người khác",
+    maNhom: "GP01",
+    ngayKhoiChieu: "2020-12-22T00:00:00",
+    danhGia: 10,
+  },
+  {
+    maPhim: 1344,
+    tenPhim: "Gia dinh",
+    biDanh: "gia-dinh",
+    trailer: "string",
+    hinhAnh: "http://movie0706.cybersoft.edu.vn/hinhanh/gia-dinh_gp01.jpg",
+    moTa: "string",
+    maNhom: "GP01",
+    ngayKhoiChieu: "2020-11-30T00:00:00",
+    danhGia: 10,
+  }
+];
 
-export default class MovieManagement extends Component {
-    render() {
-        return (
-            <div>
-                movie management
-            </div>
-        )
-    }
+function createData(id, name, pic, date) {
+  return { id, name, pic, date };
 }
+
+const rows = movieList.map((movie) =>
+  createData(movie.maPhim, movie.tenPhim, movie.hinhAnh, movie.ngayKhoiChieu)
+);
+
+const EditButton = (props) => (
+  <CssIconButton aria-label="delete" style={{ color: "orange" }}>
+    <CreateIcon />
+  </CssIconButton>
+);
+
+const DeleteButton = (props) => (
+  <CssIconButton aria-label="delete" style={{ color: "red" }}>
+    <BlockIcon />
+  </CssIconButton>
+);
+
+function MovieManagement() {
+  const classes = useStyles();
+  return (
+    <>
+      <Box my={2}>
+        <SearchBar />
+      </Box>
+
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Mã phim</StyledTableCell>
+              <StyledTableCell align="right">Tên phim</StyledTableCell>
+              <StyledTableCell align="right">Hình ảnh</StyledTableCell>
+              <StyledTableCell align="right">Ngày khởi chiếu</StyledTableCell>
+              <StyledTableCell align="right">Chỉnh sửa/Xóa</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, index) => (
+              <StyledTableRow key={index}>
+                <StyledTableCell align="right">{row.id}</StyledTableCell>
+                <StyledTableCell align="right">{row.name}</StyledTableCell>
+                <StyledTableCell align="right">{row.pic}</StyledTableCell>
+                <StyledTableCell align="right">{row.date}</StyledTableCell>
+                <StyledTableCell align="right">
+                  <EditButton />
+                  <DeleteButton />
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+
+export default MovieManagement;
---
+++ src/components/MovieManagement/style.js
@@ -0,0 +1,42 @@
+import {
+    withStyles,
+    makeStyles,
+    TableCell,
+    TableRow,
+    IconButton,
+  } from "@material-ui/core";
+  
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 700,
+    },
+  });
+  
+  export const CssIconButton = withStyles((theme) => ({
+    root: {
+      "&.MuiIconButton-root": {
+        outline:"none",
+        padding:5,
+        
+      },
+    },
+  }))(IconButton);
+  export const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.grey.dark,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  export const StyledTableRow = withStyles((theme) => ({
+    root: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+  export default useStyles;
+  
