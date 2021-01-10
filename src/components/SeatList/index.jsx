a88fdd1d92cc9ea8e192cdd1c3c63aeb96709eab
CaoThanhLong <ctlong26051993@gmail.com>
2021-01-10 13:21:18 +0700

    Cập nhật component SeatList (đổi tên ChairList thành SeatList)

--- src/components/SeatList/index.jsx
+++ src/components/SeatList/index.jsx
@@ -1,58 +1,33 @@
-import { Box, Grid } from "@material-ui/core";
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid } from "@material-ui/core";
 import Seat from "../Seat";
-import useStyles from "./style";
 
-// const chairList = [
-//   { SoGhe: 1, TenGhe: "số 1 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 2, TenGhe: "số 2 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 3, TenGhe: "số 3 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 4, TenGhe: "số 4 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 5, TenGhe: "số 5 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 6, TenGhe: "số 6 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 7, TenGhe: "số 7 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 8, TenGhe: "số 7 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 9, TenGhe: "số 9 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 10, TenGhe: "số 10 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 11, TenGhe: "số 11 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 12, TenGhe: "số 12 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 13, TenGhe: "số 13 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 14, TenGhe: "số 14 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 15, TenGhe: "số 15 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 16, TenGhe: "số 16 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 17, TenGhe: "số 17 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 18, TenGhe: "số 18 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 19, TenGhe: "số 19 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 20, TenGhe: "số 20 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 21, TenGhe: "số 21 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 22, TenGhe: "số 22 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 23, TenGhe: "số 23 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 24, TenGhe: "số 24 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 25, TenGhe: "số 25 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 26, TenGhe: "số 26 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 27, TenGhe: "số 27 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 28, TenGhe: "số 28 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 29, TenGhe: "số 29 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 30, TenGhe: "số 30 ", Gia: 100, TrangThai: true },
-//   { SoGhe: 31, TenGhe: "số 31 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 32, TenGhe: "số 32 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 33, TenGhe: "số 33 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 34, TenGhe: "số 34 ", Gia: 100, TrangThai: false },
-//   { SoGhe: 35, TenGhe: "số 35 ", Gia: 100, TrangThai: false },
-// ];
+const SeatList = ({ list }) => {
+  const RenderSeats = (type) => {
+    return list
+      .filter((seat) => seat.loaiGhe === type)
+      .map((seat) => (
+        <Grid key={seat.maGhe} item xs={1}>
+          <Seat type={type} seatNumber={seat.tenGhe} reserved={seat.daDat} />
+        </Grid>
+      ));
+  };
 
-const list = {
-  thongTinPhim: {
-    maLichChieu: 15290,
-    tenCumRap: "BHD Star Cineplex - 3/2",
-    tenRap: "Rạp 2",
-    diaChi: "L5-Vincom 3/2, 3C Đường 3/2, Q.10",
-    tenPhim: "Trainwreck",
-    hinhAnh: "http://movie0706.cybersoft.edu.vn/hinhanh/trainwreck.jpg",
-    ngayChieu: "01/01/2019",
-    gioChieu: "12:01",
-  },
-  danhSachGhe: [
+  return (
+    <>
+      <Grid container>{RenderSeats("Thuong")}</Grid>
+      <Grid container>{RenderSeats("Vip")}</Grid>
+    </>
+  );
+};
+
+SeatList.propTypes = {
+  list: PropTypes.array,
+};
+
+SeatList.defaultProps = {
+  list: [
     {
       maGhe: 47561,
       tenGhe: "01",
@@ -180,7 +155,7 @@ const list = {
       loaiGhe: "Thuong",
       stt: "13",
       giaVe: 75000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -190,7 +165,7 @@ const list = {
       loaiGhe: "Thuong",
       stt: "14",
       giaVe: 75000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -210,7 +185,7 @@ const list = {
       loaiGhe: "Thuong",
       stt: "16",
       giaVe: 75000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -260,7 +235,7 @@ const list = {
       loaiGhe: "Thuong",
       stt: "21",
       giaVe: 75000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -270,7 +245,7 @@ const list = {
       loaiGhe: "Thuong",
       stt: "22",
       giaVe: 75000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -340,7 +315,7 @@ const list = {
       loaiGhe: "Thuong",
       stt: "29",
       giaVe: 75000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -970,7 +945,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "92",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -980,7 +955,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "93",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -990,7 +965,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "94",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -1040,7 +1015,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "99",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -1050,7 +1025,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "100",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -1090,7 +1065,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "104",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -1120,7 +1095,7 @@ const list = {
       loaiGhe: "Vip",
       stt: "107",
       giaVe: 90000,
-      daDat: false,
+      daDat: true,
       taiKhoanNguoiDat: null,
     },
     {
@@ -1656,56 +1631,4 @@ const list = {
   ],
 };
 
-const NormalChair = (props) => {
-  const classes = useStyles();
-  return (
-    <Grid container>
-      {list.danhSachGhe.map((item, index) => {
-        if (item.loaiGhe === "Thuong")
-          return (
-            <Grid key={index} item xs={1}>
-              <Box m={0.5}>
-                <Chair
-                  tenGhe={item.tenGhe}
-                  loaiGhe={item.loaiGhe}
-                  daDat={item.daDat}
-                />
-              </Box>
-            </Grid>
-          );
-      })}
-    </Grid>
-  );
-};
-
-const VipChair = (props) => {
-  const classes = useStyles();
-  return (
-    <Grid container>
-      {list.danhSachGhe.map((item, index) => {
-        if (item.loaiGhe === "Vip")
-          return (
-            <Grid key={index} item xs={1}>
-              <Box m={0.5}>
-                <Seat
-                  tenGhe={item.tenGhe}
-                  loaiGhe={item.loaiGhe}
-                  daDat={item.daDat}
-                />
-              </Box>
-            </Grid>
-          );
-      })}
-    </Grid>
-  );
-};
-
-export const ChairList = (props) => {
-  const classes = useStyles();
-  return (
-    <Box className={classes.seatArea}>
-      <NormalChair />
-      <VipChair />
-    </Box>
-  );
-};
+export default SeatList;
