61d4012e914ce097e72e5605a31af64d0d74c97a
CaoThanhLong <ctlong26051993@gmail.com>
2021-01-11 11:16:10 +0700

    Cập nhật component PriceInfo (đổi tên PriceTable thành PriceInfo)

---
+++ src/components/PriceInfo/style.js
@@ -0,0 +1,77 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+
+  totalPrice: {
+    padding: theme.spacing(1, 0),
+  },
+
+  center: {
+    textAlign: "center",
+  },
+
+  right: {
+    textAlign: "right",
+  },
+
+  price: {
+    fontWeight: 600,
+    color: theme.palette.green.main,
+  },
+
+  dashedBoder: {
+    borderBottom: "1.5px dashed rgba(224, 224, 224, 1)",
+  },
+
+  showTimeInfo: {
+    padding: theme.spacing(1, 0),
+  },
+
+  seats: {
+    padding: theme.spacing(1, 0),
+    alignItems: "center",
+  },
+
+  userInfo: {
+    padding: theme.spacing(1, 0),
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  input: {
+    margin: theme.spacing(1.5, 0),
+
+    "& .MuiInputLabel-root": {
+      color: theme.palette.primary.main,
+    },
+    "& .MuiOutlinedInput-root": {
+      borderRadius: theme.shape.borderRadius,
+
+      "& fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&:hover fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+
+  button: {
+    width: "100%",
+    color: theme.palette.white,
+    margin: 0,
+    fontSize: theme.typography.subtitle2.fontSize,
+    fontWeight: theme.typography.subtitle2.fontWeight,
+    "&:focus": {
+      outline: "none",
+    },
+  },
+}));
+
+export default useStyles;
