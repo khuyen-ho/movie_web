cf6a8a0ea0f0f0de272422d295dc5f714ab1e5f3
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-01-12 16:23:14 +0700

    xóa cmt thừa

--- src/components/UserModal/index.jsx
+++ src/components/UserModal/index.jsx
@@ -93,40 +93,6 @@ const Info = (props) => {
             type="text"
           />
 
-          {/* <FormControlLabel
-            classes={{ root: styles.checkbox, label: styles.label }}
-            control={
-              <Checkbox
-                checked={changePassword}
-                onChange={() => setChangePassword(!changePassword)}
-                name="changePassword"
-              />
-            }
-            label="Đổi mật khẩu"
-          />
-
-          <Collapse in={changePassword} timeout="auto">
-            <Box className={styles.info}>
-              <TextField
-                className={styles.input}
-                label="Mật khẩu hiện tại"
-                variant="outlined"
-                type="password"
-              />
-              <TextField
-                className={styles.input}
-                label="Mật khẩu mới"
-                variant="outlined"
-                type="password"
-              />
-              <TextField
-                className={styles.input}
-                label="Xác nhận mật khẩu"
-                variant="outlined"
-                type="password"
-              />
-            </Box>
-          </Collapse> */}
         </Box>
 
         <Button variant="contained" color="secondary" className={styles.button}>
