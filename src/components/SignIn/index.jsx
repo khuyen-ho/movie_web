cae9914a913342cda061f2143d4ba079a5c8fa41
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-01-13 23:16:22 +0700

    xử lí form SignIn với Formik

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -10,58 +10,75 @@ import {
 import Switch from "@material-ui/core/Switch";
 import useStyles from "./style";
 import { NavLink } from "react-router-dom";
-
+import { Formik, Form, Field } from "formik";
 const SignIn = (props) => {
   const styles = useStyles();
 
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
   return (
-    <form className={styles.root} noValidate autoComplete="off">
-      <Box className={styles.content}>
-        <Typography className={styles.title} variant="h6">
-          ĐĂNG NHẬP
-        </Typography>
+    <Formik
+      initialValues={{
+        taiKhoan: "",
+        matKhau: "",
+      }}
+      onSubmit={handleSubmit}
+      render={(formikProps) => (
+        <Form className={styles.root} noValidate autoComplete="off">
+          <Box className={styles.content}>
+            <Typography className={styles.title} variant="h6">
+              ĐĂNG NHẬP
+            </Typography>
 
-        <TextField
-          className={styles.input}
-          label="Tên tài khoản"
-          variant="outlined"
-          type="text"
-        />
+            <TextField
+              className={styles.input}
+              label="Tên tài khoản"
+              variant="outlined"
+              type="text"
+              name="taiKhoan"
+              onChange={formikProps.handleChange}
+            />
 
-        <TextField
-          className={styles.input}
-          label="Mật khẩu"
-          variant="outlined"
-          type="password"
-        />
+            <TextField
+              className={styles.input}
+              label="Mật khẩu"
+              variant="outlined"
+              type="password"
+              name="matKhau"
+              onChange={formikProps.handleChange}
+            />
 
-        <Button
-          variant="contained"
-          color="secondary"
-          fullWidth
-          className={styles.button}
-        >
-          ĐĂNG NHẬP
-        </Button>
-
-        <Box className={styles.bottomGroup}>
-          <FormControlLabel
-            classes={{ root: styles.switch, label: styles.label }}
-            control={<Switch name="keepLoggedIn" />}
-            label="Duy trì đăng nhập"
-          />
-          <NavLink to="/signup">
-            <Link
-              href="#"
-              className={`${styles.label} ${styles.link}`}
-              variant="subtitle1"
+            <Button
+              variant="contained"
+              color="secondary"
+              fullWidth
+              className={styles.button}
+              type='submit'
             >
-              Đăng ký tài khoản
-            </Link>
-          </NavLink>
-        </Box>
-      </Box>
-    </form>
+              ĐĂNG NHẬP
+            </Button>
+
+            <Box className={styles.bottomGroup}>
+              <FormControlLabel
+                classes={{ root: styles.switch, label: styles.label }}
+                control={<Switch name="keepLoggedIn" />}
+                label="Duy trì đăng nhập"
+              />
+              <NavLink to="/signup">
+                <Link
+                  href="#"
+                  className={`${styles.label} ${styles.link}`}
+                  variant="subtitle1"
+                >
+                  Đăng ký tài khoản
+                </Link>
+              </NavLink>
+            </Box>
+          </Box>
+        </Form>
+      )}
+    />
   );
 };
 
