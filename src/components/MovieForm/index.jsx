12445d0dbc1205ac506845ed7d4e7417105c62be
CaoThanhLong <ctlong26051993@gmail.com>
2021-01-13 23:34:55 +0700

    Thêm component MovieForm (demo)

---
+++ src/components/MovieForm/index.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { Box, Typography, TextField, Button } from "@material-ui/core";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import useStyles from "./style";
+
+const MovieForm = (props) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const styles = useStyles();
+
+  return (
+    <>
+      <form noValidate autoComplete="off">
+        <Box className={styles.content}>
+          <Typography className={styles.title} component="h1" variant="h5">
+            Quản lý phim
+          </Typography>
+
+          <Box className={styles.info}>
+            <TextField
+              className={styles.input}
+              label="Mã nhóm"
+              variant="outlined"
+              type="text"
+              size="small"
+              defaultValue="GP00"
+            />
+
+            <TextField
+              className={styles.input}
+              label="Mã phim"
+              variant="outlined"
+              type="text"
+              size="small"
+              defaultValue="1283"
+            />
+
+            <TextField
+              className={styles.input}
+              label="Tên phim"
+              variant="outlined"
+              type="text"
+              size="small"
+              defaultValue="Trainwreck"
+            />
+
+            <TextField
+              className={styles.input}
+              label="Bí danh"
+              variant="outlined"
+              type="text"
+              size="small"
+              defaultValue="trainwreck"
+            />
+
+            <TextField
+              className={styles.input}
+              label="Trailer"
+              variant="outlined"
+              type="text"
+              size="small"
+              defaultValue="https://www.youtube.com/embed/2MxnhBPoIx4"
+            />
+
+            <TextField
+              className={styles.input}
+              label="Mô tả"
+              variant="outlined"
+              type="text"
+              size="small"
+              multiline
+              rowsMax={10}
+            />
+          </Box>
+
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              autoOk
+              variant="inline"
+              inputVariant="outlined"
+              size="small"
+              invalidDateMessage="Ngày không đúng định dạng"
+              maxDateMessage="Ngày chọn không được sau ngày 01/01/2100"
+              minDateMessage="Ngày chọn không được trước ngày 01/01/1900"
+              format="dd/MM/yyyy"
+              id="start-date"
+              label="Ngày khởi chiếu"
+              value={startDate}
+              onChange={(date) => setStartDate(date)}
+              className={styles.datePicker}
+            />
+          </MuiPickersUtilsProvider>
+          <TextField
+            className={styles.input}
+            label="Đánh giá"
+            variant="outlined"
+            type="number"
+            size="small"
+            defaultValue={5.0}
+          />
+
+          <TextField
+            className={styles.input}
+            label="Hình ảnh"
+            variant="outlined"
+            type="file"
+            size="small"
+          />
+        </Box>
+
+        <Button variant="contained" color="secondary" className={styles.button}>
+          Thêm
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default MovieForm;
---
+++ src/components/MovieForm/style.js
@@ -0,0 +1,99 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: theme.palette.secondary.main,
+    margin: theme.spacing(1, 0),
+    textAlign: "left",
+    fontSize: theme.typography.h6.fontSize,
+  },
+
+  content: {
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  input: {
+    margin: theme.spacing(1, 0),
+    marginRight: theme.spacing(1),
+    width: "48%",
+
+    "& .MuiInputBase-root, & .MuiInputLabel-root": {
+      fontSize: theme.typography.body2.fontSize,
+    },
+
+    "& .MuiInputLabel-root": {
+      color: theme.palette.primary.main,
+    },
+    "& .MuiOutlinedInput-root": {
+      borderRadius: theme.shape.borderRadius,
+
+      "& fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&:hover fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+
+  datePicker: {
+    margin: theme.spacing(1, 0),
+    width: "48%",
+
+    "& button": {
+      outlineStyle: "none",
+    },
+
+    "& .MuiInputBase-root, & .MuiInputLabel-root": {
+      fontSize: theme.typography.body2.fontSize,
+    },
+
+    "& .MuiInputLabel-root": {
+      color: theme.palette.primary.main,
+    },
+    "& .MuiOutlinedInput-root": {
+      borderRadius: theme.shape.borderRadius,
+
+      "& fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&:hover fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+    },
+
+    "& .MuiOutlinedInput-adornedEnd": {
+      padding: 0,
+    },
+
+    [theme.breakpoints.down("sm")]: {
+      marginRight: 0,
+    },
+  },
+
+  button: {
+    color: theme.palette.white,
+    margin: theme.spacing(1, 0),
+    fontSize: theme.typography.subtitle2.fontSize,
+    fontWeight: theme.typography.subtitle2.fontWeight,
+    "&:focus": {
+      outline: "none",
+    },
+
+    [theme.breakpoints.up("xs")]: {
+      width: "100%",
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: 150,
+    },
+  },
+}));
+
+export default useStyles;
