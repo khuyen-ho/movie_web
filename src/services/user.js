d61f53521404d4eb8cf74e688038f0047e7d3dd2
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-01-14 13:08:15 +0700

    tách service cho phần validate form

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,16 @@
 import Axios from "axios";
+import * as yup from "yup";
+export const signUpSchema = yup.object().shape({
+  taiKhoan: yup.string().required("*Bắt buộc"),
+  matKhau: yup.string().required("*Bắt buộc"),
+  email: yup.string().required("*Bắt buộc").email("*Email không hợp lệ"),
+  soDt: yup
+    .string()
+    .required("*Bắt buộc")
+    .matches(/^[0-9]+$/, "Số điện thoại không hợp lệ"),
 
+  hoTen: yup.string().required("*Bắt buộc"),
+});
 class UserService {
   signIn(data) {
     return Axios({
