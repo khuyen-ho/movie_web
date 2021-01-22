5260019855e78559ef0d1bb77e7287ef1285e185
CaoThanhLong <ctlong26051993@gmail.com>
2021-01-22 15:07:36 +0700

    Fix lại giao diện phần quick search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import { Box, InputBase, IconButton } from "@material-ui/core";
-// import SearchIcon from "@material-ui/icons/Search";
-// import useStyles from "./style";
-
-// const Search = (props) => {
-//   const styles = useStyles();
-
-//   return (
-//     <Box className={styles.root}>
-//       <InputBase
-//         placeholder={props.placeholder}
-//         fullWidth
-//         classes={{
-//           root: styles.inputRoot,
-//           input: styles.inputInput,
-//         }}
-//       />
-//       <IconButton className={styles.iconButton}>
-//         <SearchIcon className={styles.searchIcon} fontSize="large" />
-//       </IconButton>
-//     </Box>
-//   );
-// };
-
-// Search.propTypes = {
-//   placeholder: PropTypes.string,
-// };
-
-// Search.defaultProps = {
-//   placeholder: "Nhập tìm kiếm...",
-// };
-
-// export default Search;
-
 import React from "react";
 import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
@@ -89,17 +53,14 @@ const Search = ({
     </>
   );
 };
-
 Search.propTypes = {
   autoList: PropTypes.array,
   placeholder: PropTypes.string,
   dispatchType: PropTypes.string,
   searchAction: PropTypes.func,
 };
-
 Search.defaultProps = {
   autoList: [],
   placeholder: "Nhập tìm kiếm...",
 };
-
 export default Search;
