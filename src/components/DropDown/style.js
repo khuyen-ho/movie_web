3bb8011f9b4a369a0bf9e0e15cc7b5a8da362730
CaoThanhLong <ctlong26051993@gmail.com>
2021-01-22 15:14:54 +0700

    Fix lại giao diện phần advanced search

--- src/components/DropDown/style.js
+++ src/components/DropDown/style.js
@@ -3,9 +3,31 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   formControl: {
     width: "100%",
+
+    "& .MuiInputBase-root, & .MuiInputLabel-root": {
+      fontSize: theme.typography.body2.fontSize,
+    },
+
+    "& .MuiInputLabel-root": {
+      color: theme.palette.primary.main,
+    },
+    "& .MuiOutlinedInput-root": {
+      borderRadius: theme.shape.borderRadius,
+
+      "& fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&:hover fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: theme.palette.primary.main,
+      },
+    },
   },
+
   select: {
-    fontSize: theme.typography.subtitle1.fontSize,
+    fontSize: theme.typography.body2.fontSize,
     textAlign: "left",
     width: "100%",
     borderBottom: "none",
@@ -13,11 +35,13 @@ const useStyles = makeStyles((theme) => ({
       background: "none",
     },
   },
+
   icon: {
     color: theme.palette.secondary.main,
   },
+
   menuItem: {
-    fontSize: theme.typography.subtitle1.fontSize,
+    fontSize: theme.typography.body2.fontSize,
   },
 }));
 
