72051cda084ea446885ac0c07c14fedc82d134b8
CaoThanhLong <ctlong26051993@gmail.com>
2021-01-26 00:28:14 +0700

    Sửa lỗi sau khi merge fix_movie_managment

--- src/components/PersonalInfo/style.js
+++ src/components/PersonalInfo/style.js
@@ -13,12 +13,6 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
   },
 
-  content: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-  },
-
   info: {
     display: "flex",
     flexWrap: "wrap",
